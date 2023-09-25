002982618f3eea6378d214e5339c02a4b7b5b5b1
Jesus De La Cruz <jesusdaviddelacruzarroyo@yahoo.com>
2023-09-24 19:28:34 -0500

    feat ACTUALIZACION DE VISTA LOGIN

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  usernameInputFocused: boolean = false;
+  passwordInputFocused: boolean = false;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.loadScript('assets/js/input-focus.js').then(() => {
+      console.log('Script loaded successfully!');
+    }).catch(error => {
+      console.error('Script load error:', error);
+    });
+  }
+
+  private loadScript(url: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+
+      script.onload = () => {
+        resolve();
+      };
+
+      script.onerror = () => {
+        reject(new Error(`Failed to load script: ${url}`));
+      };
+
+      document.body.appendChild(script);
+    });
+  }
+  
+  login() {
+    // Aquí puedes agregar tu lógica de inicio de sesión
+    console.log('Botón de inicio de sesión clickeado');
+    // Puedes agregar tu lógica de inicio de sesión aquí
+  }
 }
