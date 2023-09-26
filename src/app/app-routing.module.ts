8cbeb3083a019e5e35601031af9540d2456e0f9f
Jesus De La Cruz <jesusdaviddelacruzarroyo@yahoo.com>
2023-09-25 22:44:58 -0500

    fix correcion de ajuste de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GaleriaComponent } from './components/galeria/galeria.component';
 import { LoginComponent } from './components/login/login.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
+import { FormsModule } from '@angular/forms';
 
 
 const routes: Routes = [
-  {path: '', component:GaleriaComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'prueba', component:PruebaComponent},
+  {path: '', component:PruebaComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,44 +1,39 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements AfterViewInit {
+export class LoginComponent {
   usernameInputFocused: boolean = false;
   passwordInputFocused: boolean = false;
+  user: string = '';
+  pass: string = '';
 
   constructor() {}
 
-  ngAfterViewInit() {
-    this.loadScript('assets/js/input-focus.js').then(() => {
-      console.log('Script loaded successfully!');
-    }).catch(error => {
-      console.error('Script load error:', error);
-    });
+  onInputFocus() {
+    this.usernameInputFocused = true;
   }
 
-  private loadScript(url: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = url;
+  onInputBlur() {
+    if (!this.user.trim()) {
+      this.usernameInputFocused = false; 
+    }  }
 
-      script.onload = () => {
-        resolve();
-      };
-
-      script.onerror = () => {
-        reject(new Error(`Failed to load script: ${url}`));
-      };
-
-      document.body.appendChild(script);
-    });
+  onPasswordInputFocus() {
+    this.passwordInputFocused = true;
   }
-  
+
+  onPasswordInputBlur() {
+    if (!this.pass.trim()) {
+      this.passwordInputFocused = false;
+    }
+  }
+
   login() {
-    // Aquí puedes agregar tu lógica de inicio de sesión
+    // Lógica de inicio de sesión
     console.log('Botón de inicio de sesión clickeado');
     // Puedes agregar tu lógica de inicio de sesión aquí
   }
