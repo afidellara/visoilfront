85d08ea9b0cfcfc24963558ccff01743cedbb7ea
Jesus De La Cruz <jesusdaviddelacruzarroyo@yahoo.com>
2023-09-27 18:52:05 -0500

    fix VISTA INICIO FINALIZADA

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   isMenuVisible = false;
+  isMenuVisible2 = false;
+  isMenuVisible3 = false;
 
   constructor() { }
 
@@ -15,5 +17,24 @@ export class MenuComponent implements OnInit {
 
   toggleMenu() {
     this.isMenuVisible = !this.isMenuVisible;
+  }
+  toggleMenu2() {
+    this.isMenuVisible2 = !this.isMenuVisible2;
+  }
+  toggleMenu3() {
+    this.isMenuVisible3 = !this.isMenuVisible3;
+  }
+  
+  getHeight(menuNumber: number): number {
+    switch (menuNumber) {
+      case 1:
+        return this.isMenuVisible ? 90 : 0;
+      case 2:
+        return this.isMenuVisible2 ? 90 : 0;
+      case 3:
+        return this.isMenuVisible3 ? 90 : 0;
+      default:
+        return 0;
+    }
   }
 }
